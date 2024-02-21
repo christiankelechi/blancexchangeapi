881c51d0591278a29a5e05f6d55197e1e203298c
christiankelechi <kezechristian@gmail.com>
2024-02-21 23:19:20 +0100

    implemented the user registration api and login api

--- core_app_root/security/auth/serializer/register.py
+++ core_app_root/security/auth/serializer/register.py
@@ -9,7 +9,7 @@ class RegisterSerializer(UserSerializer):
 
     class Meta:
         model = User
-        fields = ['id','email', 'username','first_name','last_name','country','password','confirm_password']
+        fields = ['id','email', 'username','password','confirm_password']
 
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
